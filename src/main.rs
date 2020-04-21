fbaa931f44d3e95e6a813a57c113eafa67f35807
Hadrien G <knights_of_ni@gmx.com>
2020-04-21 20:55:16 +0200

    Start building a process tree

--- src/main.rs
+++ src/main.rs
@@ -13,22 +13,23 @@ use heim::{
     host::{Arch, Pid, Platform, User},
     memory::{Memory, Swap},
     net::{Address, MacAddr, Nic},
-    process::ProcessError,
+    process::{Command, Process, ProcessError},
     sensors::TemperatureSensor,
     units::{
         frequency::megahertz,
         information::{byte, gigabyte, kilobyte, megabyte, terabyte},
         thermodynamic_temperature::degree_celsius,
-        Information, ThermodynamicTemperature as Temperature,
+        Information, ThermodynamicTemperature as Temperature, Time,
     },
     virt::Virtualization,
 };
 
-use slog::{debug, error, info, o, warn, Drain, Logger};
+use slog::{debug, info, o, warn, Drain, Logger};
 
 use std::{
-    collections::{btree_map::Entry, BTreeMap},
+    collections::{btree_map::Entry, BTreeMap, BTreeSet},
     net::{Ipv4Addr, Ipv6Addr, SocketAddr},
+    path::PathBuf,
     sync::Mutex,
 };
 
@@ -59,8 +60,8 @@ async fn main() -> heim::Result<()> {
     let disk_partitions_and_usage = heim::disk::partitions()
         .and_then(|partition| async {
             // NOTE: Failure to stat a partition is purposely treated as a
-            //       non-fatal event, as it can happen on some random
-            //       pseudo-filesystem that no one cares about.
+            //       non-fatal event, unlike all other failures, as it happens
+            //       on random pseudo-filesystems that no one cares about.
             let usage_result = heim::disk::usage(partition.mount_point()).await;
             Ok((partition, usage_result))
         })
@@ -119,49 +120,296 @@ async fn main() -> heim::Result<()> {
     let user_connections = user_connections.await?;
     report_users(&log, user_connections);
 
+    /// A node in the process tree
+    struct ProcessNode {
+        /// The heim Process object from process enumeration
+        ///
+        /// If a process is first referred to as a parent of another process,
+        /// Err(ProcessError::NoSuchProcess(pid)) will be inserted as a
+        /// placeholder value. This placeholder should eventually be replaced by
+        /// the corresponding process enumeration result, unless...
+        ///
+        /// 1. An unlucky race condition occured and a process was seen during
+        ///    process enumeration, but not the parent of that process.
+        /// 2. The process of interest is a special PID that does not actually
+        ///    map into a user-mode system process (like PID 0) on Linux.
+        //
+        // TODO: Replace with inanimate struct
+        process_info_result: Result<ProcessInfo, ProcessInfoError>,
+
+        /// Children of this process in the process tree
+        children: BTreeSet<Pid>,
+    }
+
+    /// Result of a detailed initial process info query.
+    struct ProcessInfo {
+        /// PID of the parent process
+        parent_pid: Result<Pid, ProcessInfoFieldError>,
+
+        /// Name of this process
+        name: Result<String, ProcessInfoFieldError>,
+
+        /// Path to this process' executable
+        exe: Result<PathBuf, ProcessInfoFieldError>,
+
+        /// Command line with which the process was invoked
+        command: Result<Command, ProcessInfoFieldError>,
+
+        /// Time at which the process was created, since Unix epoch
+        // TODO: Convert to something like SystemTime instead
+        create_time: Result<Time, ProcessInfoFieldError>,
+    }
+
+    /// Error which can occur while fetching a specific piece of process
+    /// information, without that invalidating the entire ProcessInfo struct.
+    //
+    // NOTE: If we got a NoSuchProcess or a ZombieProcess error, we consider
+    //       that the entire ProcessInfo is bogus: the process has exited, and
+    //       if we queried again, we'd get the same error for all info.
+    //
+    //       If we got a Load error, we consider that the entire process
+    //       enumeration process is bogus and will abort that higher-level
+    //       process. This follows the "this error is unrecoverable" design
+    //       principle of heim::Error.
+    //
+    //       This only leaves AccessDenied errors as something which solely
+    //       affects the current process info field, for now.
+    //
+    //       Also, as far as I know, when querying a process property, the
+    //       error's `pid` field can only contain the Pid of the target process.
+    //       I'll cross-check that assumption with an assertion.
+    #[non_exhaustive]
+    #[derive(Copy, Clone, Debug, Eq, PartialEq)]
+    enum ProcessInfoFieldError {
+        /// Not enough permissions to query this ProcessInfo field.
+        AccessDenied,
+    }
+
+    /// Error which invalidates the entire ProcessInfo query.
+    //
+    // NOTE: As far as I know, AccessDenied errors cannot upon while listing
+    //       processes, but only while enumerating their individual fields. But
+    //       I will cross-check that with an assertion.
+    //
+    //       Load errors are treated as fatal errors, and therefore abort the
+    //       enumeration of the entire process tree, not just the current
+    //       process.
+    //
+    //       Also, as far as I know, when enumerating processes or querying
+    //       their properties, the error's `pid` field can only contain the Pid
+    //       of the target process. I'll cross-check that assumption with an
+    //       assertion whenever possible.
+    #[non_exhaustive]
+    #[derive(Copy, Clone, Debug, Eq, PartialEq)]
+    enum ProcessInfoError {
+        /// The process exited during the query and doesn't exist anymore.
+        NoSuchProcess,
+
+        /// Same as above, but the parent didn't reclaim its exit status yet.
+        ZombieProcess,
+    }
+
+    /// Starting from a Result of the process enumeration process, try to fetch
+    /// as much process info as possible, and produce a report on that.
+    ///
+    /// The output is a three-layered Result cake:
+    ///
+    /// - In the worst case, process enumeration failed badly and we couldn't
+    ///   even get a Pid. In that case, aborting the enumeration is recommended.
+    /// - Even if we did get a Pid, we may not get more than that because the
+    ///   target process has exited and all info was lost. In that case, the
+    ///   enumeration returns the Pid and a process exit error.
+    /// - Finally, we may succeed in querying some info about a process but not
+    ///   all of it because we do not have enough permissions. This is
+    ///   particularly frequent when querying daemons. In that case, only the
+    ///   corresponding ProcessInfo field will error out.
+    ///
+    async fn get_process_info(
+        enumeration_result: Result<Process, ProcessError>
+    ) -> heim::Result<(Pid, Result<ProcessInfo, ProcessInfoError>)> {
+        match enumeration_result {
+            // Process was correctly enumerated
+            Ok(process) => {
+                // Get its PID
+                let pid = process.pid();
+
+                // TODO: And now, this is the tricky part where we query
+                //       individual process info, and if it fails we try to
+                //       return the right error in the right place. The logic
+                //       is going to be pretty gnarly, so better factor it out
+                //       in a macro!
+                //
+                //       Once we have a macro, we'll "only" need to call it
+                //       once per field and build the final ProcessInfo struct,
+                //       then return that as a Ok((pid, Ok(process_info)))
+                //
+
+                // And now, we're going to query ProcessInfo fields one by one,
+                // and here the error handling will get a little hairy...
+                macro_rules! get_info_field {
+                    ($field_name:ident) => {
+                        match process.$field_name().await {
+                            // If we got the info, we bubble it up happily
+                            Ok(info) => Ok(info),
+
+                            // If we got an AccessDenied error, we expect the
+                            // received pid to match the process we queried, and
+                            // we consider that this failure only affects the
+                            // active ProcessInfo field.
+                            Err(ProcessError::AccessDenied(res_pid)) => {
+                                assert_eq!(res_pid, pid);
+                                Err(ProcessInfoFieldError::AccessDenied)
+                            }
+
+                            // If we got a NoSuchProcess or a ZombieProcess
+                            // error, we consider it to invalidate the entire
+                            // ProcessInfo struct, but still bubble up the Pid.
+                            Err(ProcessError::NoSuchProcess(res_pid)) => {
+                                assert_eq!(res_pid, pid);
+                                return Ok((pid, Err(ProcessInfoError::NoSuchProcess)))
+                            }
+                            Err(ProcessError::ZombieProcess(res_pid)) => {
+                                assert_eq!(res_pid, pid);
+                                return Ok((pid, Err(ProcessInfoError::ZombieProcess)))
+                            }
+
+                            // If we got a Load error, we treat it as an
+                            // unrecoverable heim failure, as advised by the
+                            // documentation, and thus we abort the entire
+                            // process enumeration query.
+                            Err(ProcessError::Load(err)) => return Err(err),
+
+                            // Since heim uses nonexhaustive enums, we must
+                            // error out at runtime instead of at compile time
+                            _ => unimplemented!("Unsupported process query error"),
+                        }
+                    }
+                }
+
+                // Once we know how to get a ProcessInfo struct field, we know
+                // how to get the whole ProcessInfo struct.
+                macro_rules! get_info_struct {
+                    ( $($field_name:ident),* ) => {
+                        Ok((
+                            pid,
+                            Ok(ProcessInfo {
+                                $( $field_name: get_info_field!($field_name) ),*
+                            })
+                        ))
+                    }
+                }
+                get_info_struct!(parent_pid, name, exe, command, create_time)
+            }
+
+            // Process doesn't exist anymore, most likely some kind of race
+            // condition happened during enumeration.
+            Err(ProcessError::NoSuchProcess(pid)) =>
+                Ok((pid, Err(ProcessInfoError::NoSuchProcess))),
+
+            // Process is a zombie (it has exited, so every info but its status
+            // was discarded, the status itself will be discarded once the
+            // parent process waits for that)
+            Err(ProcessError::ZombieProcess(pid)) =>
+                Ok((pid, Err(ProcessInfoError::ZombieProcess))),
+
+            // Not enough permission to get a Process struct (?)
+            Err(ProcessError::AccessDenied(_pid)) =>
+                // As far as I know, this error cannot happen. The Process type
+                // is basically a thin Pid wrapper, so if you don't have enough
+                // permissions to get the wrapper, you shouldn't have enough
+                // permissions to get a Pid either, and thus a Load error should
+                // occur, not an AccessDenied one. Therefore it doesn't make
+                // sense to account for this error in ProcessInfoError.
+                unreachable!(),
+
+            // Unrecoverable heim failure upon loading process data, we didn't
+            // even manage to get the process' Pid.
+            Err(ProcessError::Load(err)) => Err(err),
+
+            // Since heim uses nonexhaustive enums, we must
+            // error out at runtime instead of at compile time
+            _ => unimplemented!("Unsupported process query error"),
+        }
+    }
+
+
+    // Initialize the process tree
+    let mut process_tree = BTreeMap::<Pid, ProcessNode>::new();
+
     // TODO: Report on processes, highlighting this one and maybe trying to
     //       trace each down to a user login process or PID 1 too.
     let my_pid = std::process::id() as i32;
     let processes_results = processes_results.await;
     for process_result in processes_results {
-        match process_result {
-            Ok(process) => {
-                let process_log = log.new(o!("pid" => process.pid()));
+        // TODO: Do this as a stream map, rather than after awaiting the stream.
+        //       This will reduce the amount of async code.
+        let (pid, process_info_result) = get_process_info(process_result).await?;
+        let process_log = log.new(o!("pid" => pid));
+
+        match &process_info_result {
+            Ok(process_info) => {
+                // If we could query the process' parent, add ourselves to its
+                // list of children in the process tree.
+                if let Ok(parent_pid) = process_info.parent_pid {
+                    let insert_result =
+                        process_tree
+                            .entry(parent_pid)
+                            .or_insert(ProcessNode {
+                                process_info_result: Err(ProcessInfoError::NoSuchProcess),
+                                children: BTreeSet::new(),
+                            })
+                            .children
+                            .insert(pid);
+                    assert!(!insert_result, "Observed the same process twice!");
+                }
+
+                // DEBUG: Now, log basic process info
                 info!(process_log, "Found a process";
                       // TODO: Build a process tree instead of just printing
                       //       parent PID right away.
                       // FIXME: Go beyond debug repr, ideally try to aggregate
                       //        some errors like nonexistent or zombie too.
-                      // TODO: Translate faulty ZombieProcess error from cwd()
-                      //       into the AccessError that it usually is.
-                      "parent pid" => ?process.parent_pid().await,
-                      "name" => ?process.name().await,
-                      "executable path" => ?process.exe().await,
-                      "command line" => ?process.command().await,
-                      "working directory" => ?process.cwd().await,
-                      "status" => ?process.status().await,
-                      "creation time" => ?process.create_time().await,
-                      "running" => ?process.is_running().await);
-                if process.pid() == my_pid {
-                    debug!(process_log, "It's-a Me!");
+                      "parent pid" => ?process_info.parent_pid,
+                      "name" => ?process_info.name,
+                      "executable path" => ?process_info.exe,
+                      "command line" => ?process_info.command,
+                      "creation time" => ?process_info.create_time);
+
+                // DEBUG: Point ourselves out
+                if pid == my_pid {
+                    debug!(process_log, "It's-a Me! BenchMon!");
                 }
             }
-            Err(ProcessError::NoSuchProcess(pid)) => {
-                debug!(log, "Found a process, but it seems to have vanished";
-                       "pid" => pid)
+
+            Err(ProcessInfoError::NoSuchProcess) => {
+                debug!(process_log, "Found a process, but then it vanished");
             }
-            Err(ProcessError::ZombieProcess(pid)) => {
-                warn!(log, "Found a zombie process";
-                      "pid" => pid)
+
+            Err(ProcessInfoError::ZombieProcess) => {
+                warn!(process_log, "Found a process in the zombie state");
             }
-            Err(ProcessError::AccessDenied(pid)) => {
-                error!(log, "Found a process that I'm not allowed to query";
-                       "pid" => pid)
+
+        }
+
+        // Fill that process' info in the process tree
+        match process_tree.entry(pid) {
+            // No entry yet, process was seen before its children or has none
+            Entry::Vacant(vacant_entry) => {
+                vacant_entry.insert(ProcessNode {
+                    process_info_result,
+                    children: BTreeSet::new(),
+                });
             }
-            Err(ProcessError::Load(err)) => Err(err)?,
-            _ => unimplemented!("Unsupported process query error")
+
+            // Existing entry from previously seen children, fill in main info
+            Entry::Occupied(occupied_entry) => {
+                occupied_entry.into_mut().process_info_result = process_info_result;
+            }
         }
     }
+
+    // TODO: Print out the process tree using a fancy hierarchical format
+
 
     // TODO: Extract this system summary to a separate async fn, then start
     //       polling useful "dynamic" quantities in a system monitor like
